9d444dcb69fd3644b11f27edcb5d5d4591ac2c35
zero-24 <zero-24@users.noreply.github.com>
2017-09-30 16:20:12 +0200

    space

--- script.php
+++ script.php
@@ -6,7 +6,7 @@
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
 
- defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
 /**
  * Installation class to perform additional changes during install/uninstall/update
