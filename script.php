45217b9f6f9af732236626368c042a8b49d166f1
zero-24 <zero-24@users.noreply.github.com>
2019-02-18 19:09:39 +0100

    2018 -> 2019 && .sys.ini vs .ini

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 /**
  * HttpHeader Plugin
  *
- * @copyright  Copyright (C) 2017 - 2018 Tobias Zulauf All rights reserved.
+ * @copyright  Copyright (C) 2017 - 2019 Tobias Zulauf All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
  */
 
